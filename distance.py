42473a11d8e18f663981b156b5ee7c5baf322cd0
Tee Barnes <balteebar@gmail.com>
2022-03-23 22:50:48 -0400

    refactored code

--- distance.py
+++ distance.py
@@ -1,22 +1,30 @@
 # Write a function that takes in 2 points (2 tuples) on a coordinate axes, so
 # (x, y). This function should then return the distance between them. Hint:
 # Think of the pythagorean theorem a^2 + b^2 = c^2.
-
-import math
 # Write a function that takes in 2 points (2 tuples)
 
+# full breakdown
+#import math
+# def distancetup(x, y):
+#     x = (x[0], x[1])  # 2 points(2 tuples) on a coordinate axes, (x, y)
+#     y = (y[0], y[1])
 
-def distancetup(x, y):
-    x = (x[0], x[1])  # 2 points(2 tuples) on a coordinate axes, (x, y)
+#     # This function should then return the distance between them.
+#     a = (x[0]-x[1])**2 + (y[0]-y[1])**2
+#     print(a)
+#     distance = math.sqrt(a)  # Distance Formula
+#     return distance
+# t=(5, 3)
+# p=(4,5)
+# distancetup(t,p)
+
+import math
+
+
+def distance(x, y):
+    x = (x[0], x[1])
     y = (y[0], y[1])
-
-    # This function should then return the distance between them.
-    a = (x[0]-x[1])**2 + (y[0]-y[1])**2
-    print(a)
-    distance = math.sqrt(a)  # Distance Formula
-    return distance
+    return math.sqrt((x[0]-x[1])**2 + (y[0]-y[1])**2)
 
 
-t = (5, 3)
-p = (4, 5)
-distancetup(t, p)
+distance((5, 3), (4, 5))
